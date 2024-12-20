249e6c63da2514fecf093795b5dc42b444ed6b5d
Ankit Bahl <abbahl@amazon.com>
2024-12-19 21:51:18 -0800

    fix bug with port being empty in prod

--- src/Login.js
+++ src/Login.js
@@ -3,7 +3,7 @@ const Login = () => {
 
   const login = () => {
     let baseUrl = `${window.location.protocol}//${window.location.hostname}`;
-    if (window.location.port !== '443') {
+    if (window.location.port.length > 0) {
       baseUrl = `${baseUrl}:${window.location.port}`;
     }
     window.location.replace(`https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=${encodeURIComponent(`${baseUrl}/login-redirect`)}&prompt=consent&response_type=code&client_id=710411787393-6oke2o3ae0j2ekq51hqkkdofteneufrl.apps.googleusercontent.com&scope=https://mail.google.com/&access_type=offline`)
